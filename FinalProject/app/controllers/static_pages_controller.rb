aa99f89f58c2c732fc77088d80e2aa3197e95ab3
mclacs14 <bradleyg36@gmail.com>
2014-12-09 10:15:11 -0500

    Added Controllers

---
+++ FinalProject/app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def login
+  end
+
+  def signup
+  end
+
+  def draftpicks
+  end
+
+  def picklist
+  end
+end
--- FinalProject/config/routes.rb
+++ FinalProject/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'static_pages/login'
+
+  get 'static_pages/signup'
+
+  get 'static_pages/draftpicks'
+
+  get 'static_pages/picklist'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
